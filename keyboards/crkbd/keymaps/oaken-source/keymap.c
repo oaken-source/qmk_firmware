ecc603d05d64a284c67a411769b2ae9628916850
Andreas Grapentin <andreas@grapentin.org>
2023-07-28 14:35:21 +0200

    moved keymap definition to separate file to prepare for model split

--- keyboards/crkbd/keymaps/oaken-source/keymap.c
+++ keyboards/crkbd/keymaps/oaken-source/keymap.c
@@ -20,55 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 #include <keymap_german.h>
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, DE_SCLN, DE_COMM,  DE_DOT,    DE_P,    DE_Y,                         DE_F,    DE_G,    DE_C,    DE_R,    DE_L, DE_SLSH,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    DE_A,    DE_O,    DE_E,    DE_U,    DE_I,                         DE_D,    DE_H,    DE_T,    DE_N,    DE_S, DE_MINS,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, DE_QUOT,    DE_Q,    DE_J,    DE_K,    DE_X,                         DE_B,    DE_M,    DE_W,    DE_V,    DE_Z, KC_BSPC,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(1),  KC_SPC,     KC_ENT,   MO(2), KC_RALT
-                                      //`--------------------------'  `--------------------------'
-  ),
-
-  [1] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    DE_1,    DE_2,    DE_3,    DE_4,    DE_5,                         DE_6,    DE_7,    DE_8,    DE_9,    DE_0,   DE_SS,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, DE_ADIA, DE_ODIA, XXXXXXX, DE_UDIA, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX,  KC_ESC,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, KC_BSPC,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT,   MO(3), KC_RALT
-                                      //`--------------------------'  `--------------------------'
-  ),
-
-  [2] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, DE_AMPR, DE_LBRC, DE_LCBR, DE_RCBR, DE_LPRN,                       DE_EQL, DE_ASTR, DE_RPRN, DE_PLUS, DE_RBRC, DE_EXLM,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, DE_TILD,  DE_DLR,  DE_DEG, XXXXXXX, XXXXXXX,                      XXXXXXX, DE_PIPE, DE_PERC, DE_CIRC,   DE_AT, DE_BSLS,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,  DE_GRV, DE_SECT, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DE_HASH, KC_BSPC,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(3),  KC_SPC,     KC_ENT, _______, KC_RALT
-                                      //`--------------------------'  `--------------------------'
-  ),
-
-  [3] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
-                                      //`--------------------------'  `--------------------------'
-  )
-};
+#include "keymap.def"
 
 const key_override_t comm_key_override = ko_make_with_layers(MOD_MASK_SHIFT, DE_COMM, DE_LABK, 1<<4);
 const key_override_t dot_key_override = ko_make_with_layers(MOD_MASK_SHIFT, DE_DOT, DE_RABK, 1<<4);
@@ -278,7 +230,7 @@ static void print_status_narrow(void) {
 
     /* Print current model */
     oled_write("MODEL", false);
-    oled_write("andi ", false);
+    oled_write(MODEL, false);
 
     /* Print current layer */
     oled_set_cursor(0, 3);
